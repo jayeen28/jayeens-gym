4922ae0d8ddf1ef2d282966b0e683a48bb17c5ae
MD. Jayeen Bin Alam <jayeen6783@gmail.com>
2021-10-19 06:07:51 +0600

    404 page added

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './Error.css'
 const Error = () => {
     return (
-        <div>
-
+        <div className="error-page">
+            <h1>404 Error</h1>
+            <h4>No page found</h4>
+            <Link to="/home">Go back to home page</Link>
         </div>
     );
 };
