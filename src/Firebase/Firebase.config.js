9266e4555cf26f3475669facda4dcd002bb552ba
MD. Jayeen Bin Alam <jayeen6783@gmail.com>
2021-10-25 15:48:44 +0600

    environment variable used

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyATovablZ1CElyiOX-xzEm3tw0GFhdROyc",
-    authDomain: "jayeen-s-gym.firebaseapp.com",
-    projectId: "jayeen-s-gym",
-    storageBucket: "jayeen-s-gym.appspot.com",
-    messagingSenderId: "828614312967",
-    appId: "1:828614312967:web:ea8e720043a82530d956a2"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 export default firebaseConfig;
